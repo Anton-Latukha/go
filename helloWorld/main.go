9cc6f41396fdcb2583077571504fd27fe327cfec
Anton-Latukha <anton.latukha@gmail.com>
2017-03-07 00:03:02 +0200

    Naming numbers

--- helloWorld/main.go
+++ helloWorld/main.go
@@ -14,14 +14,14 @@ func main() {
   fmt.Scanf("%i", &number)
   switch number {
     case 0: fmt.Println("Zero")
-    case 1: fmt.Println("Zero")
-    case 2: fmt.Println("Zero")
-    case 3: fmt.Println("Zero")
-    case 4: fmt.Println("Zero")
-    case 5: fmt.Println("Zero")
-    case 6: fmt.Println("Zero")
-    case 7: fmt.Println("Zero")
-    case 8: fmt.Println("Zero")
-    case 9: fmt.Println("Zero")
+    case 1: fmt.Println("One")
+    case 2: fmt.Println("Two")
+    case 3: fmt.Println("Three")
+    case 4: fmt.Println("Four")
+    case 5: fmt.Println("Five")
+    case 6: fmt.Println("Six")
+    case 7: fmt.Println("Seven")
+    case 8: fmt.Println("Eight")
+    case 9: fmt.Println("Nine")
   }
 }
