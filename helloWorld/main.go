569de4de1246208e79944206a81c001c217fdf1b
Anton-Latukha <anton.latukha@gmail.com>
2017-03-07 02:28:29 +0200

    Adding program prettyness.

--- helloWorld/main.go
+++ helloWorld/main.go
@@ -6,16 +6,18 @@ func main() {
   
   // Loop section
   for i:=0 ; i<2 ; i++ {
-    fmt.Print("String",i + 1,":")
+    fmt.Print("String",i + 1,": ")
     var name string
     fmt.Scanf("%s", &name)
-    fmt.Println("Hello!", name)
+    fmt.Println("Hello! ", name)
   }
   
   // Switch section
   fmt.Println("Now, send me a number!")
   var number int
   fmt.Scanf("%d", &number)
+  
+  fmt.Print("It is... ")
   switch number {
     case 0: fmt.Println("Zero")
     case 1: fmt.Println("One")
@@ -31,6 +33,9 @@ func main() {
   }
   
   // Arrays/Slices
+  
+  fmt.Println("Now lets' talk about seasons")
+  
   month := [...]string {1:"January", 2:"February", 3:"March", 4:"April", 5:"May", 6:"June", 7:"July", 8:"August", 9:"September", 10:"October", 11:"November", 12:"December"}
   Q1 := month[1:4]
   Q2 := month[4:7]
@@ -39,16 +44,17 @@ func main() {
   winter := month[1:3]
   spring := month[3:6]
   summer := month[6:9]
-  autumn := month[9:13]
+  autumn := month[9:12]
   
-  fmt.Println(Q1)
-  fmt.Println(Q2)
-  fmt.Println(Q3)
-  fmt.Println(Q4)
   
-  fmt.Println(winter)
-  fmt.Println(spring)
-  fmt.Println(summer)
-  fmt.Println(autumn)
+  fmt.Println("Q1 is: ", Q1)
+  fmt.Println("Q2 is: ", Q2)
+  fmt.Println("Q3 is: ", Q3)
+  fmt.Println("Q4 is: ", Q4)
+  
+  fmt.Println("North Winter is: ", winter)
+  fmt.Println("North Spring: ", spring)
+  fmt.Println("North Summer: ", summer)
+  fmt.Println("North Autumn: ", autumn)
   
 }
