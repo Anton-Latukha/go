db7a5023005d9b7dc076ed3990522808c45a490b
Anton-Latukha <anton.latukha@gmail.com>
2017-03-07 02:22:15 +0200

    Creating slices: year quarters, year seasons

--- helloWorld/main.go
+++ helloWorld/main.go
@@ -32,4 +32,23 @@ func main() {
   
   // Arrays/Slices
   month := [...]string {1:"January", 2:"February", 3:"March", 4:"April", 5:"May", 6:"June", 7:"July", 8:"August", 9:"September", 10:"October", 11:"November", 12:"December"}
+  Q1 := month[1:4]
+  Q2 := month[4:7]
+  Q3 := month[7:10]
+  Q4 := month[10:13]
+  winter := month[1:3]
+  spring := month[3:6]
+  summer := month[6:9]
+  autumn := month[9:13]
+  
+  fmt.Println(Q1)
+  fmt.Println(Q2)
+  fmt.Println(Q3)
+  fmt.Println(Q4)
+  
+  fmt.Println(winter)
+  fmt.Println(spring)
+  fmt.Println(summer)
+  fmt.Println(autumn)
+  
 }
