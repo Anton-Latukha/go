6a3de1c43d64d7f8f58de1cd6890b3782403e858
Anton-Latukha <anton.latukha@gmail.com>
2017-03-07 22:37:03 +0200

    Adding program prettyness.

---
+++ cod_task1/main.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+import "encoding/binary"
+
+func main() {
+
+  N := 2147483647
+  
+  // if N not in range - throw exception
+  if N < 1 && N > 2147483647 {
+    fmt.Println ("Received N in invalid range")
+  }
+  
+  number := uint32(N)
+  var binBuf [32]byte
+  binLen := PutUvarint(binBuf, number) // int
+  
+  yesBitPositionsArr := [33]int
+  yesBitPositions := yesBitPositionsArr[]
+  var bitPositionsCell int
+  
+  for i:=0 i<(binLen-1) i+1 {
+    
+    if binBuf[i] == 1 {
+      
+      yesBitPositions = appendInt(yesBitPositions, i)
+    
+    }
+    
+    if cap(yesBitPositions) == len(yesBitPositions) {fmt.Println("Slice yesBitPositions is full!")}
+    
+    
+  }
+  
+}
